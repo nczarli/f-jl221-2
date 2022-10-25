dce4c55af215802b41eeae5951e12a2d24b09b99
Nikodem Czarlinski <nczarlinski@gmail.com>
2022-10-25 10:26:50 +0100

    correct images

--- Hand Label Images/label.py
+++
@@ -1,6 +0,0 @@
-
-
-# Rule Based Algorithm\Sample Photos\Pits\Cherries,20,19-08-2022,18-10-00,pass.bmp
-""" 
-labelme Rule Based Algorithm\Sample Photos\Pits\Cherries,20,19-08-2022,18-10-00,pass.bmp \
-    -O Cherries,20,19-08-2022,18-10-00,pass.bmp.json """
